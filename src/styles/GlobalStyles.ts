c0ceacdcaeea630038493bbe3e29c0f0b29630b4
Luan Alves <akaluan.la@gmail.com>
2021-07-15 14:55:34 -0300

    adiocionas novas media queries

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -10,20 +10,20 @@ export const GlobalStyle = createGlobalStyle`
 
   }
   * {
-    font-size: 18px;
     margin: 0;  
     padding: 0;
     box-sizing: border-box;
   }
 
   html {
-    @media (max-width: 1080px) {
-      font-size: 93.75%;
+    @media (max-width: 1200px) {
+      font-size: 87.5%;
     }
 
-    @media (max-width: 720px) {
-      font-size: 87.5%;
-    }  
+    @media (max-width: 990px) {
+      font-size: 60%;
+    } 
+
   }
 
   body {
