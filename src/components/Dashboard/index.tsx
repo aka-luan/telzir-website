898b49cb3393827698d50521ee5c20528a534209
Luan Alves <akaluan.la@gmail.com>
2021-07-15 14:56:19 -0300

    Medias queries para lidar com a responsividade

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,25 +1,82 @@
+import React from 'react'
+import { DashboardState } from '../../utils/interfaces'
 import { Form } from '../Form'
 import { Container } from './styles'
 
-export function Dashboard() {
-  return (
-    <Container>
-      <div className="left-container">
-        <h1>Com o FaleMais você fala de graça!</h1>
-        <p>Com o novo produto FaleMais da Telzir você pode falar de graça até
-          um determinado tempo (em minutos) e só paga os minutos excedentes.
-          Calcule o preço das ligações no formulário ao lado.</p>
-        {/* <p>Escolha o código das cidade de origem e destino, informe o tempo da ligação e selecione qual o
-          plano FaleMais.</p> */}
-        <div className="buttons-container">
-          <button>Assine agora</button>
-          <button>Ver vantagens</button>
+export class Dashboard extends React.Component<{}, DashboardState> {
+  constructor (props: any) {
+    super(props)
+
+    this.state = {
+      leftStyles: {
+        visibility: 'visible',
+        opacity: 1,
+        right: '0'
+      },
+      rightStyles: {
+      },
+
+      isForm: false
+    }
+  }
+
+  handleClick = () => {
+
+    if(!this.state.isForm) {
+      this.setState({
+        leftStyles: {
+          visibility: 'hidden',
+          opacity: 0,
+          right: '10rem'
+        },
+        rightStyles: {        
+          opacity: 1,
+          visibility: 'visible',
+        },
+
+        isForm: !this.state.isForm
+      })
+    } else {
+      this.setState({
+        leftStyles: {
+          visibility: 'visible',
+          opacity: 1,
+          right: '0'
+        },
+        rightStyles: {        
+          opacity: 0,
+          visibility: 'hidden',
+        },
+        isForm: !this.state.isForm
+      })
+    }
+
+    
+  }
+
+  render() {
+    return (
+      <Container>
+        <div className="form-toggle" >
+          <div onClick={this.handleClick}>{this.state.isForm? 'Voltar' : 'Calcular'}</div>
         </div>
-      </div>
-      <div className="center-column"></div>
-      <div className="right-container">
-        <Form />
-      </div>
-    </Container>
-  )
+        <div className="left-container" style={this.state.leftStyles}>
+          <h1>Com o FaleMais você fala de graça!</h1>
+          <p>Com o novo produto FaleMais da Telzir você pode falar de graça até
+            um determinado tempo (em minutos) e só paga os minutos excedentes.
+            Calcule o preço das ligações no formulário.</p>
+          {/* <p>Escolha o código das cidade de origem e destino, informe o tempo da ligação e selecione qual o
+            plano FaleMais.</p> */}
+          <div className="buttons-container">
+            <button>Assine agora</button>
+            <button>Ver vantagens</button>
+          </div>
+        </div>
+        <div className="center-column"></div>
+        <div className="right-container" style={this.state.rightStyles}>
+          <Form />
+        </div>
+      </Container>
+    )
+  }  
 }
--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -2,21 +2,27 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   margin: 0 auto;
+  width: 90%;
   max-width: 1600px;
   display: flex;
   margin-top: 4rem;
 
+  .form-toggle {
+    visibility: hidden;
+  }
+
   .left-container {
+    transition: visibility 1s, opacity 1s, right 1s ;
     width: 70%;
     padding-left: 5rem;
-
+    display: flex;
+    flex-direction: column;
+    position: relative;
 
     h1 {
       font-weight: 500;
       font-size: 4.25rem;
-      word-spacing: 0%;
-      line-height: 1.25;
-      
+      line-height: 1.25;      
     }
 
     p {
@@ -25,12 +31,18 @@ export const Container = styled.div`
     }
 
     .buttons-container {
+      display: flex;
+      
       button {
         height: 4rem;
         width: 10rem;
         background: var(--black);
         color: var(--buttonText);
         border: none;
+
+        @media (max-width: 880px) {
+          height: 5rem;
+        }
       }      
 
       button:last-child {
@@ -49,9 +61,96 @@ export const Container = styled.div`
     width: 5rem;
     background-color: black;
     margin-left: 5rem;
+    position: relative;
   }
 
   .right-container {
+    
     width: 70%;
+  }
+
+  /* media queries */
+
+  @media (max-width: 540px) {
+    flex-direction: column;
+
+    .form-toggle {
+      visibility: visible;
+      margin: 1rem;
+      cursor: pointer;
+      height: 5rem;
+      
+      div {
+        transition: all 1s ease-out;
+        padding: 1rem;
+        background-color: var(--yellow);
+        color: var(--black);
+        text-align: center;
+        width: 8rem;
+        font-weight: 500;
+        float: right;
+        font-size: 1.25rem;
+      }
+    }
+
+    .left-container {
+      
+      place-self: center;
+      text-align: center;
+      padding-left: 0;
+      margin-left: 0;
+
+      p {
+        font-size: 1.25rem;
+        place-self: center;
+        max-width: 40rem;
+      }
+
+      .buttons-container {
+        justify-content: center;
+      }
+    }
+
+    .right-container {      
+      transition: all 1s ease-in-out;
+      position: absolute;
+      place-self: center;
+      margin-top: 7rem;
+      visibility: hidden;
+      opacity: 0;
+
+      .img-container {
+        display: none;
+      }
+
+      div {
+        margin-left: 0;
+      }
+    }
+
+    .center-column {
+      width: 100% ;
+      height: 1rem;
+      margin-left: 0;
+    }
+
+    .tarifas-container {
+      width: auto;
+    }
+  }
+  @media (max-width: 540px) {
+
+    .left-container {
+      width: 100%;
+
+    }
+    
+    .right-container {
+      width: 90%;
+    }
+
+    .tarifas-container {
+      width: 80%;
+    }
   }
 `
