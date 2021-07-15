3075dbd36e83185446b9640d45f971e1d6b2a286
Luan Alves <akaluan.la@gmail.com>
2021-07-15 14:56:25 -0300

    Medias queries para lidar com a responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,55 @@
 import { Container } from './styles'
 import logoImg from '../../assets/logo.png'
-export function Header() {
-  return (
 
+export function Header() {  
+  
+
+  window.addEventListener('load', () => {
+    let show = true;
+    const menuContainer = document.querySelector('.nav-menu-container') as Element
+    const menuToggle = document.querySelector('.menu-toggle') as Element
+
+  menuToggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menuContainer?.classList.toggle('on', show)
+
+    show = !show
+  })
+  })
+
+  
+
+  return (
     <Container>
       <div className="nav-logo">
         <img src={logoImg} alt="Tezir Logo" />
-        <span>Telzir</span>
       </div>
-      <ul className="nav-menu-items">
-        <li>
-         <a href="/">FaleMais</a>
-        </li>
-        <li>
-        <a href="/">Soluções</a>
-        </li>
-        <li>
-        <a href="/">Sobre Nós</a>
-        </li>
-        <li>
-        <a href="/">Contatos</a>
-        </li>
-        <li>
-        <a href="/">Preços</a>
-        </li>      
-      </ul>
-
+      <div className="nav-menu-container">
+        <div className="menu-toggle">
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="three"></div>
+        </div>
+        <ul className="nav-menu-items">
+          <li>
+            <a href="/">FaleMais</a>
+          </li>
+          <li>
+            <a href="/">Soluções</a>
+          </li>
+          <li>
+            <a href="/">Sobre Nós</a>
+          </li>
+          <li>
+            <a href="/">Contatos</a>
+          </li>
+          <li>
+            <a href="/">Preços</a>
+          </li>
+        </ul>
+      </div>
     </Container>
 
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,16 +3,15 @@ import styled from 'styled-components'
 export const Container = styled.header`
   margin: 0 auto;
   height: 5rem;
-  display: flex;
+  display: flex;  
   max-width: 1700px;
   position: relative;
+  width: 100%;
 
   .nav-logo {
     display: flex;
-
-    img {
-    margin: 1rem 1rem;
-  }
+    height: 4rem;
+    width: 90%;
   }
   
   .nav-menu-items {
@@ -34,7 +33,6 @@ export const Container = styled.header`
           color: var(--buttonText);
           transition: all 1s;
         }
-
       }
     }
     
@@ -42,10 +40,89 @@ export const Container = styled.header`
       text-align: center;
       line-height: 3rem;
       color: var(--black);
-      font-weight: 400;
+      font-weight: 500;
       margin: 0 2rem;
       text-decoration: none;   
     }
+  }
+
+  /* media queries*/
+
+  @media(max-width: 540px) {
+    justify-content: space-between;
+
+    .nav-logo {
+      width: 0;
+      margin: 10px;
+    }
+    
+    .nav-menu-items {
+      display: none;
+    }
+
+    .one,
+    .two,
+    .three {
+      background-color: var(--black);
+      height: 5px;
+      width: 100%;
+      margin: 6px auto;
+      
+      transition-duration: 0.3s;
+    }
+
+    .menu-toggle {
+      width: 40px;
+      height: 30px;
+      margin-right: 20px;
+      margin-top: 20px;
+    }
+
+    /* FullScreen */
+    .nav-menu-container.on{
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      width: 100vw;
+      height: 100vh;
+
+      background-color: var(--yellow);
+
+      z-index: 10;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .menu-toggle {
+        position: absolute;
+        right: 0;
+        top: 0;
+
+        .one {
+          transform: rotate(45deg) translate(7px, 7px);
+        }
+        .two {
+          opacity: 0;
+        }
+        .three {
+          transform: rotate(-45deg) translate(8px, -9px);
+        }
+      }
+
+      ul {
+        position: relative;
+        display: grid;
+        text-align: center;
+
+        a {
+            transition-duration: 0.5s;
+            font-size: 3rem;
+            line-height: 7rem;
+        }
+      }
+    }
+
   }
 
 `
