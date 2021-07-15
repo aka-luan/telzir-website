acd974156444f8d30add5d1854c121dd7df386c3
Luan Alves <akaluan.la@gmail.com>
2021-07-15 14:54:38 -0300

    adc novas estilizações e card de tarifas

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,77 +1,151 @@
 import React, { SyntheticEvent } from 'react'
-import { NewForm } from './styles'
+import { Container } from './styles'
 import { constants } from '../../utils/constants'
-import { MyState } from '../../utils/interfaces'
+import { FormState } from '../../utils/interfaces'
+import vLine from '../../assets/round line.svg'
+import hLine from '../../assets/round line2.svg'
+import womanImg from '../../assets/woman.png'
 
 
-export class Form extends React.Component<{},  MyState >{
+export class Form extends React.Component<{}, FormState> {
   constructor(props: any) {
-    super(props) 
+    super(props)
 
     this.state = {
-      origem: "011",
-      destino: "016",
-      minutos: "00",
-      plano: "30",
-      tarifaFaleMais: "0",
-      tarifaPadrao: "0"
+      formData: {
+        origem: '011',
+        destino: '016',
+        minutos: '0',
+        plano: '30',
+        tarifaFaleMais: '0',
+        tarifaPadrao: '0'
+      },
+      formStyles: {
+        opacity: 1,
+        left: '0'
+      },
+      tarifaStyles: {
+        visibility: 'hidden',
+        opacity: 0,
+        left: '40%'
+      },
+      isForm: true
     }
   }
 
   handleInputChange = (event: { target: { name: any, value: any } }) => {
     const name = (event.target as HTMLInputElement).name
     const value = (event.target as HTMLInputElement).value
-    const newState = { [name]: value } 
-
-    this.setState(newState as Pick<MyState, keyof MyState>)    
+    let newState = this.state.formData
+    newState[name] = value
+    this.setState({ formData: newState })
   }
 
 
   handleSubmit = (event: SyntheticEvent) => {
-    const origem = this.state.origem
-    const destino = this.state.destino
-    const tipoTarifa = `TARIFA${origem}_${destino}`    
+    console.log(parseInt(this.state.formData.minutos))
+    if (parseInt(this.state.formData.minutos) < 0){      
+      alert('Insira um valor positivo')      
+      event.preventDefault()      
+      return 
+    } else if (isNaN(parseInt(this.state.formData.minutos))) {
+      alert('Insira um valor válido')
+      event.preventDefault()
+      return
+    } else if (this.state.isForm) {
+      this.setState({ isForm: !this.state.isForm })
+    } else {
+      this.setState({
+        formStyles: {
+          visibility: 'visible',
+          opacity: 1,
+          left: '0'
+        },
+        tarifaStyles: {
+          visibility: 'hidden',
+          opacity: 0,
+          left: '40%'
+        },
+        isForm: !this.state.isForm
+      })
+      event.preventDefault()
+      return
+    }
+
+    const origem = this.state.formData.origem
+    const destino = this.state.formData.destino
+    const tipoTarifa = `TARIFA${origem}_${destino}`
     const tarifa = constants.tarifas[tipoTarifa]
-    const minutos = parseInt(this.state.minutos)
-    const plano = parseInt(this.state.plano)
+    const minutos = parseInt(this.state.formData.minutos)
+    const plano = parseInt(this.state.formData.plano)
+
+    let newState = this.state.formData
+    newState.tarifaPadrao = ((minutos * tarifa)).toFixed(2).toString()
+    newState.tarifaFaleMais = Math.max(0, ((((minutos - plano) * tarifa) * 1.1))).toFixed(2).toString()
 
     this.setState({
-      tarifaPadrao: ((minutos * tarifa)).toFixed(2).toString(),
-      tarifaFaleMais: parseInt(this.state.tarifaFaleMais) < 0?  '0' : ((((minutos - plano) * tarifa) * 1.1)).toFixed(2).toString()
+      formData: newState,
+      formStyles: {
+        visibility: 'hidden',
+        opacity: 0,
+        left: '3rem'
+      },
+      tarifaStyles: {
+        visibility: 'visible',
+        opacity: 1,
+        left: '50%'
+      }
     })
 
+    console.log(this.state.formData)
     event.preventDefault()
   }
 
   render() {
     return (
-      <NewForm onSubmit={this.handleSubmit}>
-        
-        <label htmlFor="codOrigem">Código de origem</label>
-        <select name="origem" id="codOrigem" value={this.state.origem} onChange={this.handleInputChange}>
-          <option value="011">011</option>
-          <option value="016">016</option>
-          <option value="017">017</option>
-          <option value="018">018</option>
-        </select>
-        <label htmlFor="codDestino">Código de destino</label>
-        <select name="destino" id="codDestino" value={this.state.destino} onChange={this.handleInputChange}>
-          <option value="011">011</option>
-          <option value="016">016</option>
-          <option value="017">017</option>
-          <option value="018">018</option>
-        </select>
-        <label htmlFor="tempo">Minutos da ligação</label>
-        <input type="text" id="tempo" name="minutos" placeholder="Ex: 30, 40, 100..." onChange={this.handleInputChange}/>
-        <label htmlFor="tipoPlano">Plano FaleMais</label>
-        <select name="plano" id="tipoPlano" value={this.state.plano} onChange={this.handleInputChange}>
-          <option value="30"  >FaleMais 30</option>
-          <option value="60">FaleMais 60</option>
-          <option value="120">FaleMais 120</option>
-        </select>
-        <button type="submit">Calcular</button>
-        <div>Calcule o custo das ligações</div>
-      </NewForm>
+      <Container>
+        <form id="tarifa-form" onSubmit={this.handleSubmit} style={this.state.formStyles} >
+          <label htmlFor="codOrigem">Código de origem</label>
+          <select name="origem" id="codOrigem" value={this.state.formData.origem} onChange={this.handleInputChange}>
+            <option value="011">011</option>
+            <option value="016">016</option>
+            <option value="017">017</option>
+            <option value="018">018</option>
+          </select>
+          <label htmlFor="codDestino">Código de destino</label>
+          <select name="destino" id="codDestino" value={this.state.formData.destino} onChange={this.handleInputChange}>
+            <option value="011">011</option>
+            <option value="016">016</option>
+            <option value="017">017</option>
+            <option value="018">018</option>
+          </select>
+          <label htmlFor="tempo">Minutos da ligação</label>
+          <input type="text" id="tempo" name="minutos" placeholder="Ex: 30, 40, 100..." onChange={this.handleInputChange} />
+          <label htmlFor="tipoPlano">Plano FaleMais</label>
+          <select name="plano" id="tipoPlano" value={this.state.formData.plano} onChange={this.handleInputChange}>
+            <option value="30"  >FaleMais 30</option>
+            <option value="60">FaleMais 60</option>
+            <option value="120">FaleMais 120</option>
+          </select>
+        </form>
+        <div className="tarifas-container" style={this.state.tarifaStyles}>
+          <div className="tarifaPadrao">
+            <span>Sem FaleMais</span>
+            <span>${this.state.formData.tarifaPadrao}</span>
+          </div>
+          <div className="tarifaFaleMais">
+            <span>Com FaleMais</span>
+            <span>${this.state.formData.tarifaFaleMais}</span>
+          </div>
+        </div>        
+        <div className="img-container">
+          <img src={womanImg} alt="Mulher falando ao telefone" />
+        </div>
+        <button type="submit" form="tarifa-form">{this.state.isForm ? 'Calcular tarifas' : 'Voltar'}</button>
+        <div className="form-header"><p>Calcule o custo das ligações</p></div>
+        <div className="v-line"><img src={vLine} alt="" /></div>
+        <div className="h-line"><img src={hLine} alt="" /></div>
+      </Container>
     )
   }
 }
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,28 +1,97 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const NewForm = styled.form`
+export const Container = styled.div`
   background-color: var(--purple);
   margin-left: 2.5rem;  
-  height: 80%;
   position: relative;
-  display: flex;
-  flex-direction: column;
   padding: 0rem 3rem;
-  margin-top: 1.5rem;
+  margin-top: 1.5rem;  
 
-  div {
+  form {
+    transition: visibility 1s, opacity 1s, left 1s ease-out;
+    height: 32.5rem;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+
+  .form-header {
     text-align  : center;
-    font-size: 0.9rem;
+    font-weight: 500;
     top: -1.5rem;
     left: 1.5rem;
     position: absolute;
     background-color: var(--yellow);
     width: 8rem;
     height: 3rem;
+
+    p {
+      font-size: 0.9rem;
+    }
+  }
+
+  .tarifas-container {
+    width: 80%;
+    display: flex;
+    flex-direction: row-reverse;
+    position: absolute;
+    left: 50%;
+    top: 35%;
+    transform: translate(-50%, -50%);
+    transition: all 1s;
+
+    div {
+      width: 15rem;
+      height: 12.5rem;
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      margin: 1rem;
+      background-color: var(--yellow);
+
+      span {
+        font-weight: 500;
+        padding-top: 2rem;
+
+        &:last-child {
+          max-width: 100%;
+          overflow: hidden;
+          text-align: center;
+          font-size: 2rem;
+        }
+      }
+      
+      &:first-child {
+        color: var(--buttonText);
+        background-color: var(--black);
+      }
+    }
+  }
+
+  .v-line { 
+    top: 1rem ;
+    right: -1rem;
+    position: absolute;
+
+    img {
+      transform: rotate(3deg);
+      float: right;
+      width: 2.65rem;       
+    }    
+  }
+  .h-line {
+    top: 16rem;
+    right: 4rem;
+    position: absolute;
+
+    img {
+      transform: rotate(-89deg);
+      width: 27.4rem;
+    }    
   }
 
   label {
-    font-size: 0.9rem;
+    font-size: 1rem;
     
     &:first-child {
       margin-top: 3rem;
@@ -30,6 +99,7 @@ export const NewForm = styled.form`
   }
 
   input, select {
+    z-index: 1;
     font-size: 0.9rem;
     padding: 0.5rem 0.5rem;
     
@@ -45,11 +115,28 @@ export const NewForm = styled.form`
   }
 
   button {
-    margin-top: 2rem;
-    align-self: flex-end;
+    z-index: 1;
+    position: absolute;
+    bottom: 0;
+    right: 3rem;
     height: 4rem;
     width: 10rem;
     background: var(--black);
     color: var(--buttonText);
-  }      
+  }
+
+  .img-container {
+      z-index: 1;
+      background-color: var(--yellow);
+      height: 23rem;
+      width: 20rem;
+      top: 24rem;
+      position: absolute;
+      left: -12.5rem;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
 `
