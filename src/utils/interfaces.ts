a5946540e6102e670b341254db7372ac3debbcf7
Luan Alves <akaluan.la@gmail.com>
2021-07-15 14:54:59 -0300

    adicionadas novas interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,9 +1,29 @@
-export interface MyState {
-  [k: string]: string;
+export interface FormState {
+  formData: {
+    [k: string]: string;
+  }
+  formStyles: {
+    [k: string]: string | number;
+  }
+  tarifaStyles: {
+    [k: string]: string | number;
+  }
+  isForm: boolean;  
 }
 
 export interface Constants {
   tarifas: {
     [k: string]: number;
   }  
+}
+
+export interface DashboardState {
+  leftStyles: {
+    [k: string]: string | number;
+  }
+  rightStyles: {
+    [k: string]: string | number;
+  }
+
+  isForm: boolean;  
 }
